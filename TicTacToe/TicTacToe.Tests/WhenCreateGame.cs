7f19d2277dbfcad56dd29a0a71eaaf98a670fea7
dasha <d.bayanova@dodopizza.com>
2019-05-29 16:50:47 +0300

    Extract EmptyCell()

--- TicTacToe/TicTacToe.Tests/WhenCreateGame.cs
+++ TicTacToe/TicTacToe.Tests/WhenCreateGame.cs
@@ -13,19 +13,23 @@ namespace TicTacToe.Tests
             
             var cellAt_1_1 = field.GetCell(new Row(1), new Column(1));
             
-            var emptyCell = new Cell(" ");
+            var emptyCell = EmptyCell();
             Assert.AreEqual(emptyCell, cellAt_1_1);
         }
 
         [Test]
-        public void Cell_1_2_IsEmpty()
-        {
+        public void Cell_1_2_IsEmpty()        {
             var field = new TicTacToeGame().Field();
             
             var cellAt_1_2 = field.GetCell(new Row(1), new Column(2));
             
             var emptyCell = new Cell(" ");
             Assert.AreEqual(emptyCell, cellAt_1_2);
+        }
+
+        private static Cell EmptyCell()
+        {
+            return new Cell(" ");
         }
     }
 }
